9eb8ba8aeddffbb16f040397223933b2eef5fd3d
ZZ17390 <kaeru_yuya@hotmail.com>
2019-12-16 19:46:47 +0900

    research pc commit

--- JsonManageAXG/src/scenario/ScenarioAnalize.java
+++ JsonManageAXG/src/scenario/ScenarioAnalize.java
@@ -6,22 +6,25 @@
 package scenario;
 
 import extract.SyaryoObjectExtract;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author kaeru
  */
 public class ScenarioAnalize {
-    
+
     public static void main(String[] args) {
         /* 例として下記を解析
            冷却水OH -> エンジン故障
-        */
+         */
         SyaryoObjectExtract soe = new SyaryoObjectExtract("json", "komatsuDB_PC200_Form");
         soe.setUserDefine("config\\PC200_user_define.json");
-        
+
         ScenarioBlock.setSyaryoObjectExtract(soe);
         ScenarioBlock sc = new ScenarioBlock("root");
         ScenarioBlock sc1 = new ScenarioBlock("エンジンオイル");
@@ -31,37 +34,50 @@ public class ScenarioAnalize {
         sc1.setNEXT(sc2);
         sc2.setNEXT(sc3);
         testPrint(sc);
-        
+
         time(sc);
     }
-    
-    public static void time(ScenarioBlock start){
-        while((start = start.getNEXT()) != null){
-            System.out.println(start.item);
-            extract(start);
-        }
+
+    public static void time(ScenarioBlock start) {
+        //車両ID + シナリオ(各部品のリスト)
+        Map<String, List<List<String>>> sidTimes = extract(start);
     }
-    
-    private static Map<String, List<String>> extract(ScenarioBlock block){
-        block.data.forEach(System.out::println);
+
+    private static Map<String, List<List<String>>> extract(ScenarioBlock block) {
+        Map<String, List<List<String>>> map = new HashMap<>();
+        while ((block = block.getNEXT()) != null) {
+            Map<String, List<String>> dmap = block.data.parallelStream()
+                                                .collect(Collectors.groupingBy(d -> d.split(",")[0]));
+            dmap.entrySet().stream().forEach(d ->{
+                if(map.get(d.getKey()) == null)
+                    map.put(d.getKey(), new ArrayList<>());
+                map.get(d.getKey()).add(d.getValue());
+            });
+        }
+        
+        map.entrySet().stream().forEach(m ->{
+            System.out.println(m.getKey());
+        });
+        
         return null;
     }
-    
-    public static void testPrint(ScenarioBlock sc){
+
+    public static void testPrint(ScenarioBlock sc) {
         getBlock("", sc);
     }
-    
-    public static void getBlock(String s, ScenarioBlock block){
-        if(block != null){
-            if(s.equals("-"))
-                System.out.print(s+block.item);
-            else if(s.equals("|"))
-                System.out.print(s+block.item);
-            else
+
+    public static void getBlock(String s, ScenarioBlock block) {
+        if (block != null) {
+            if (s.equals("-")) {
+                System.out.print(s + block.item);
+            } else if (s.equals("|")) {
+                System.out.print(s + block.item);
+            } else {
                 System.out.println(block.item);
-            
-            getBlock("|",block.getOR());
-            getBlock("-",block.getAND());
+            }
+
+            getBlock("|", block.getOR());
+            getBlock("-", block.getAND());
             getBlock("", block.getNEXT());
         }
     }
